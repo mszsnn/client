49e17f5d7da5c48162b105bc57f1a82675257542
Your Name <you@example.com>
2017-12-02 13:50:16 +0800

    asdf

--- src/components/index/index.vue
+++ src/components/index/index.vue
@@ -1,22 +1,28 @@
 <template>
       <div id="index">
-
+          <div>{{data}}</div>
+          <side></side>
+          <img src="./logo.png" alt="">
       </div>
 </template>
 <script>
+    import side from "../public/side.vue"
     export default {
       name:'index',
       data(){
         return {
-
+            data:'abc'
         }
       },
+      components:{
+          side
+      }, 
       methods:{
 
       },
       mounted(){
-           this.$http.get('/api/test').then((val)=>{
-             console.log(val);
+           this.$http.get('/api/test',{aa:1}).then((val)=>{
+             this.data=val.body;
            })
       }
     }
@@ -26,6 +32,7 @@
       #index{
         width: $width;
         height:200px;
+        background-image: url('./logo.png');
       }
 
 </style>
---
+++ src/components/public/side.vue
@@ -0,0 +1,24 @@
+<template>
+      <div id="side">
+          asdfas
+      </div>
+</template>
+<script>
+  
+    export default {
+      name:'side',
+      data(){
+        return {
+           
+        }
+      },
+     
+      methods:{
+
+      },
+    }
+</script>
+<style lang="scss" scoped="">
+     
+
+</style>
