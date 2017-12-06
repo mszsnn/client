50fa2d185d98c7e3e437bfc2b00ba43a2d789486
18735108906 <291315268@qq.com>
2017-12-06 22:42:06 +0800

    wq

--- src/components/case/case_list.vue
+++ src/components/case/case_list.vue
@@ -2,12 +2,23 @@
     <div id="case">
         <div class="case_container">
             <header>
-                <div class="case_title">
-                </div>
-                <div class="case_add"></div>
+                <t :data='obj'></t>
             </header>
             <div class="case_main">
                 <swiper :options="swiperOption">
+                    <swiper-slide>
+                    <router-link class="case_session" :to="{name:'case_detail'}">
+                        <div class="case_imgbox">
+                            <img src="./case_items.png" alt="">
+                        </div>
+                        <div class="case_bottom">
+                            <div class="case_add1"><span>|</span><span>|</span><span>|</span></div>
+                            <div class="case_num">01</div>
+                            <div class="case_name">山大君宇网</div>
+                            <div class="case_eng">shandajunyuwang</div>
+                        </div>
+                    </router-link>
+                </swiper-slide>
                     <swiper-slide>
                         <router-link class="case_session" :to="{name:'case_detail'}">
                             <div class="case_imgbox">
@@ -15,63 +26,50 @@
                             </div>
                             <div class="case_bottom">
                                 <div class="case_add1"><span>|</span><span>|</span><span>|</span></div>
-                                <div class="case_num">01</div>
+                                <div class="case_num">02</div>
                                 <div class="case_name">山大君宇网</div>
                                 <div class="case_eng">shandajunyuwang</div>
                             </div>
                         </router-link>
                     </swiper-slide>
                     <swiper-slide>
-                        <div class="case_session">
+                        <router-link class="case_session" :to="{name:'case_detail'}">
                             <div class="case_imgbox">
                                 <img src="./case_items.png" alt="">
-                            </div>    
+                            </div>
                             <div class="case_bottom">
                                 <div class="case_add1"><span>|</span><span>|</span><span>|</span></div>
-                                <div class="case_num">01</div>
+                                <div class="case_num">03</div>
                                 <div class="case_name">山大君宇网</div>
                                 <div class="case_eng">shandajunyuwang</div>
                             </div>
-                        </div>
+                        </router-link>
                     </swiper-slide>
                     <swiper-slide>
-                        <div class="case_session">
+                        <router-link class="case_session" :to="{name:'case_detail'}">
                             <div class="case_imgbox">
                                 <img src="./case_items.png" alt="">
                             </div>
                             <div class="case_bottom">
                                 <div class="case_add1"><span>|</span><span>|</span><span>|</span></div>
-                                <div class="case_num">01</div>
+                                <div class="case_num">04</div>
                                 <div class="case_name">山大君宇网</div>
                                 <div class="case_eng">shandajunyuwang</div>
                             </div>
-                        </div>
+                        </router-link>
                     </swiper-slide>
                     <swiper-slide>
-                        <div class="case_session">
+                        <router-link class="case_session" :to="{name:'case_detail'}">
                             <div class="case_imgbox">
                                 <img src="./case_items.png" alt="">
                             </div>
                             <div class="case_bottom">
                                 <div class="case_add1"><span>|</span><span>|</span><span>|</span></div>
-                                <div class="case_num">01</div>
+                                <div class="case_num">05</div>
                                 <div class="case_name">山大君宇网</div>
                                 <div class="case_eng">shandajunyuwang</div>
                             </div>
-                        </div>
-                    </swiper-slide>
-                    <swiper-slide>
-                        <div class="case_session">
-                            <div class="case_imgbox">
-                                <img src="./case_items.png" alt="">
-                            </div>
-                            <div class="case_bottom">
-                                <div class="case_add1"><span>|</span><span>|</span><span>|</span></div>
-                                <div class="case_num">01</div>
-                                <div class="case_name">山大君宇网</div>
-                                <div class="case_eng">shandajunyuwang</div>
-                            </div>
-                        </div>
+                        </router-link>
                     </swiper-slide>
                 </swiper>
             </div>
@@ -81,6 +79,7 @@
 <script>
     import {swiper, swiperSlide} from 'vue-awesome-swiper';
     import 'swiper/dist/css/swiper.css';
+    import t from  "../public/title";
     export default {
         name: 'team',
         data(){
@@ -89,7 +88,12 @@
                     slidesPerView: 3,
                     spaceBetween: 20,
                     autoPlay: 1000,
-                    loop: true
+                    loop: true,
+
+                },
+                obj: {
+                    h1: 'Case study',
+                    h2: '案例展示'
                 }
             }
         },
@@ -98,12 +102,18 @@
         },
         components: {
             swiper,
-            swiperSlide
+            swiperSlide,
+            t
         },
 
     }
 </script>
 <style scoped="">
+    a {
+        color: #000;
+        text-decoration: none;
+    }
+
     #case {
         width: 100%;
         height: 100%;
@@ -116,6 +126,7 @@
 
     .case_container header {
         width: 100%;
+        margin-top:84px;
     }
 
     .case_container .case_title {
@@ -145,9 +156,9 @@
         height: 458px;
         background: #fff;
         margin: 0 auto;
-        margin-top: 31px;
         border: 1px solid #e4e5e5;
         position: relative;
+        display: block;
     }
 
     .case_container .case_imgbox {
@@ -161,7 +172,7 @@
 
     .case_container .case_bottom {
         width: 282px;
-        height: 212px;
+        height: 207px;
         transition: height .5s ease-in;
         position: absolute;
         bottom: 0;
@@ -188,28 +199,33 @@
         transition: all .5s cubic-bezier(1, 0, 0, 1);
         opacity: 0;
     }
-    .case_container .case_add1 span:nth-child(2){
+
+    .case_container .case_add1 span:nth-child(2) {
         position: absolute;
         width: 7px;
         height: 16px;
         left: 0;
-        top:0;
+        top: 0;
         color: #ccc;
         text-align: center;
         line-height: 16px;
         transition: all .5s cubic-bezier(1, 0, 0, 1);
     }
+
     .case_session:hover span:first-child {
         top: 0;
         opacity: 1;
     }
+
     .case_session:hover span:nth-child(2) {
         top: -1000px;
         opacity: 0;
     }
-    .case_session:hover .case_imgbox{
-        filter:none;
+
+    .case_session:hover .case_imgbox {
+        filter: none;
     }
+
     .case_container .case_add1 span:last-child {
         position: absolute;
         width: 8px;
@@ -217,7 +233,7 @@
         left: -1000px;
         top: 0;
         transform: rotate(90deg);
-        color:#fff;
+        color: #fff;
         text-align: center;
         line-height: 16px;
         transition: all .5s cubic-bezier(1, 0, 0, 1);
@@ -231,8 +247,8 @@
 
     .case_session:hover .case_bottom {
         height: 300px;
-        background:orange;
-        color:#fff;
+        background: orange;
+        color: #fff;
     }
 
     .case_container .case_num {
