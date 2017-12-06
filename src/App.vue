e83b2bfde6cbaf61569cb5ac518a3a99dfc686d4
nn <307585280@qq.com>
2017-12-06 13:44:35 +0800

    asdf

--- src/App.vue
+++ src/App.vue
@@ -6,66 +6,67 @@
         <router-view/>
       </transition>  
     </div>  
-   
   </div>
 </template>
 
 <script>
-import side from './components/public/side.vue'
+import side from "./components/public/side.vue";
 export default {
-  name: 'app',
-  components:{
+  name: "app",
+  components: {
     side
   }
-}
+};
 </script>
 
 <style>
-  *{
-    padding:0;
-    margin:0;
-    list-style: none;
-    font-family: 微软雅黑;
-  }
-  #app{
-    padding-left:190px;
-    width:100%;
-    height:100vh;
-    position: relative;
-    overflow: hidden;
-    box-sizing: border-box;
-  }
-  @font-face {
-    font-family: 'cg';
-    src: url('./assets/CenturyGothic.ttf');
-  }
-  @font-face {
-    font-family: 'pf';
-    src: url('./assets/PingFang Light.ttf');
-  }
+* {
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  font-family: 微软雅黑;
+}
+#app {
+  padding-left: 190px;
+  width: 100%;
+  height: 100vh;
+  position: relative;
+  overflow: hidden;
+  box-sizing: border-box;
+}
+@font-face {
+  font-family: "cg";
+  src: url("./assets/CenturyGothic.ttf");
+}
+@font-face {
+  font-family: "pf";
+  src: url("./assets/PingFang Light.ttf");
+}
 
-  .move-enter-active, .move-leave-active {
-    transition: transform 1s cubic-bezier(0.86, 0, 0.07, 1) ,filter .1s 1s , opacity 1s cubic-bezier(0.86, 0, 0.07, 1);
-  }
-  .move-enter {
-    transform:translate3d(0,100%,0);
-    filter:blur(2px);
-    opacity:0
-  }
-  .move-leave-to{
-    transform:translate3d(0,-100%,0);
-    filter:blur(2px);
-    opacity:0
-  }
-  .ms_con{
-    width:100%;
-    height:100%;
-    position:relative;
-  }
-  .ms_con > div[id]{
-      position:absolute;
-      left:0;
-      top:0;
-      transform-style:preserve-3d;
-  }
+.move-enter-active,
+.move-leave-active {
+  transition: transform 0.6s cubic-bezier(0.86, 0, 0.07, 1), filter 0.1s 0.6s,
+    opacity 0.6s cubic-bezier(0.86, 0, 0.07, 1);
+}
+.move-enter {
+  transform: translate3d(0, 100%, 0);
+  filter: blur(1px);
+  opacity: 0;
+}
+.move-leave-to {
+  transform: translate3d(0, -100%, 0);
+  filter: blur(1px);
+  opacity: 0;
+}
+.ms_con {
+  width: 100%;
+  height: 100%;
+  position: relative;
+}
+.ms_con > div[id] {
+  position: absolute;
+  left: 0;
+  top: 0;
+  transform-style: preserve-3d;
+}
 </style>
