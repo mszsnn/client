f14c7d130c2a68493304d98642df8da2a0e24e5f
nn <307585280@qq.com>
2017-12-08 13:13:28 +0800

    'asd'

--- src/App.vue
+++ src/App.vue
@@ -45,17 +45,15 @@ export default {
 
 .move-enter-active,
 .move-leave-active {
-  transition: transform 0.6s cubic-bezier(0.86, 0, 0.07, 1), filter 0.1s 0.6s,
+  transition: transform 0.6s cubic-bezier(0.86, 0, 0.07, 1), 
     opacity 0.6s cubic-bezier(0.86, 0, 0.07, 1);
 }
 .move-enter {
   transform: translate3d(0, 100%, 0);
-  filter: blur(1px);
   opacity: 0;
 }
 .move-leave-to {
   transform: translate3d(0, -100%, 0);
-  filter: blur(1px);
   opacity: 0;
 }
 .ms_con {
